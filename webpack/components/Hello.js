21a4616ab765cd9f12d26db11bd9567674e8509c
Utku Ege Tuluk <utku@wiredcraft.com>
2019-07-18 18:55:30 +0800

    testing the first version

---
+++ webpack/components/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Hello.scss';
+
+class Hello extends Component {
+ render() {
+ return (
+ <div className="test">Hey, y’alal!!</div>
+ )
+ }
+}
+export default Hello;
