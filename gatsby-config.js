dcd1d02bbafbbaea40b0540a951ba48e653efcee
Utku Ege Tuluk <utku@wiredcraft.com>
2019-07-24 16:48:41 +0800

    header and offline pwa added

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,5 +26,39 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Utku Ege Tuluk - Portfolio`,
+        short_name: `Portfolio`,
+        description: `This is the page for my portfolio.`,
+        lang: `en`,
+        start_url: `/`,
+        background_color: `#F8C053`,
+        theme_color: `#F8C053`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        localize: [
+          {
+            start_url: `/tr/`,
+            lang: `tr`,
+            name: "Utku Ege Tuluk - Portfolyo",
+            short_name: `Portfolyo`,
+            description: `Portfolyom için websitesi.`,
+          },
+          {
+            start_url: `/zh/`,
+            lang: `zh`,
+            name: "陆胜 - 投资组合",
+            short_name: `投资组合`,
+            description: `是一个包括我的投资组合的网站。`,
+          }
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { injectIntl } from "gatsby-plugin-intl"
-
+import { Helmet } from "react-helmet"
 import Navbar from "../components/Navbar"
 
 import("./index.scss")
@@ -8,9 +8,14 @@ import("./index.scss")
 export default injectIntl(
     ({ intl }) => {
         return (
-        <>
+        <>  
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>{intl.formatMessage({ id: "title" })}</title>
+            <link rel="canonical" href="https://uetuluk.xyz/en" />
+        </Helmet>
             <Navbar />
-            <div className="index">{intl.formatMessage({ id: "title" })}</div>
+            <div className="index">{intl.formatMessage({ id: "index.title" })}</div>
         </>
         )
     })
