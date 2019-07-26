250b80d020bb621492da201ba865a9509da64067
Utku Ege Tuluk <utku@wiredcraft.com>
2019-07-26 18:54:42 +0800

    resume import data

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -2,12 +2,20 @@ import React from "react"
 import { injectIntl } from "gatsby-plugin-intl"
 import Navbar from "../../components/Navbar"
 import SEO from "../../components/SEO"
+import { StaticQuery, graphql } from "gatsby"
 import("../../styles/index.scss")
 import("./index.scss")
 
 const fieldList = [
-    ""
-]
+    "Experience",
+    "Education",
+    "Extracurricular Activities",
+    "Certification",  
+    "Skills",
+    "Interests",
+];
+
+
 export default injectIntl(
     ({ intl }) => {
         return (
@@ -17,6 +25,41 @@ export default injectIntl(
             <div className="Background">
                 <div className="Container">
                     <h1>{intl.formatMessage({ id: "resume.title" })}</h1>
+                    <div>{intl.formatMessage({ id: "resume.Open All" })}</div>
+                    {fieldList.map((title)=> {
+                        return (
+                            <div key={title} className="ContentContainer">
+                                <h2>{intl.formatMessage({ id: "resume." + title })}</h2>
+                                <div className="Content">
+                                    <StaticQuery
+                                        query={
+                                            graphql`
+                                                query MyQuery {
+                                                    enYaml {
+                                                        resume {
+                                                            Certification
+                                                            Education
+                                                            Experience
+                                                            Extracurricular_Activities
+                                                            Interests
+                                                            Skills
+                                                        }
+                                                    }
+                                                }
+                                            `
+                                        }
+                                        render={data => (
+                                            // console.log(data.enYaml.resume[title.replace(/\s/g, '_')])
+                                            data.enYaml.resume[title.replace(/\s/g, '_')].map((element => {
+                                                return <p>{element}</p>
+                                            }))
+                                        )}
+                                    />
+                                </div>
+                            </div>
+                        ) 
+
+                    })}
                 </div>
             </div>
         </>
