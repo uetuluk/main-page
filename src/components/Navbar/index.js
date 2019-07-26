ddb677e77fae697a88d0f78d7d43d44558e575c8
Utku Ege Tuluk <utku@wiredcraft.com>
2019-07-26 11:54:30 +0800

    fix problem with location

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { injectIntl } from "gatsby-plugin-intl"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import LanguageButton from "../LanguageButton"
+import { Location } from '@reach/router';
 import("./index.scss")
 
 const positionList = {
@@ -11,8 +12,8 @@ const positionList = {
     "about": 3
 }
 
-const positionDecider = (linkTo) => {
-    const currentPath = window.location.pathname.split("/")[2];
+const positionDecider = (linkTo ,location) => {
+    const currentPath = location.pathname.split("/")[2];
     if (positionList[linkTo] > positionList[currentPath] ){
         return "left"
     } else {
@@ -23,15 +24,20 @@ const positionDecider = (linkTo) => {
 export default injectIntl(
     ({ intl }) => {
         return (
-        <div className="Navbar">
-            <div className="siteTitle"><AniLink cover bg="#F8C053" direction={positionDecider("resume")} to={"/" + intl.locale + "/"}>{intl.formatMessage({ id: "name" })}</AniLink></div>
-            <div className="Navbar-menu">
-                <span><AniLink cover bg="#F8C053" direction={positionDecider("resume")} to={"/" + intl.locale + "/resume/"}>{intl.formatMessage({ id: "navbar.resume" })}</AniLink></span>
-                <span><AniLink cover bg="#F8C053" direction={positionDecider("portfolio")} to={"/" + intl.locale + "/portfolio/"}>{intl.formatMessage({ id: "navbar.portfolio" })}</AniLink></span>
-                <span><AniLink cover bg="#F8C053" direction={positionDecider("about")} to={"/" + intl.locale + "/about/"}>{intl.formatMessage({ id: "navbar.about" })}</AniLink></span>
-                <span><a href="https://blog.uetuluk.xyz">{intl.formatMessage({ id: "navbar.blog" })}</a></span>
-                <LanguageButton locale={intl.locale} />
-            </div>
-        </div>
+        <Location>
+            {({location}) => 
+                <div className="Navbar">
+                    <div className="siteTitle"><AniLink cover bg="#F8C053" direction={positionDecider("resume", location )} to={"/" + intl.locale + "/"}>{intl.formatMessage({ id: "name" })}</AniLink></div>
+                    <div className="Navbar-menu">
+                        <span><AniLink cover bg="#F8C053" direction={positionDecider("resume", location )} to={"/" + intl.locale + "/resume/"}>{intl.formatMessage({ id: "navbar.resume" })}</AniLink></span>
+                        <span><AniLink cover bg="#F8C053" direction={positionDecider("portfolio", location )} to={"/" + intl.locale + "/portfolio/"}>{intl.formatMessage({ id: "navbar.portfolio" })}</AniLink></span>
+                        <span><AniLink cover bg="#F8C053" direction={positionDecider("about", location )} to={"/" + intl.locale + "/about/"}>{intl.formatMessage({ id: "navbar.about" })}</AniLink></span>
+                        <span><a href="https://blog.uetuluk.xyz">{intl.formatMessage({ id: "navbar.blog" })}</a></span>
+                        <LanguageButton locale={intl.locale} />
+                    </div>
+                </div>
+            }
+            
+        </Location>
         )
     })
