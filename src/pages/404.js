51461bbc79ac581c8c3864e59ce23955860712d0
Utku Ege Tuluk <utku@wiredcraft.com>
2019-07-30 11:56:56 +0800

    styling 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,23 @@
 import React from "react"
+import { injectIntl } from "gatsby-plugin-intl"
+import Navbar from "../components/Navbar"
+import SEO from "../components/SEO"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+
 import("../styles/index.scss")
 
-export default () => <div className="404">Boo!</div>
+export default injectIntl(
+    ({ intl }) => {
+        return (
+        <>
+            <SEO locale={intl.locale} title={intl.formatMessage({ id: "404.title" })} />  
+            <Navbar />
+            <div className="Background">
+                <div className="Container">
+                    <h1 >{intl.formatMessage({ id: "404.title" })}</h1>
+                    <h2 >{intl.formatMessage({ id: "404.subtitle" })}</h2>
+                </div>
+            </div>
+        </>
+        )
+    })
